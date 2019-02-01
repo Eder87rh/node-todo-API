a96b8faa4bcf7bba4c24b8fa7d07c2a7e04bce7f
Eder Ramirez <documentador1@telecomo.com.mx>
2019-02-01 12:48:54 -0600

    DELETE todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,4 +1,5 @@
 let mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
--- server/server.js
+++ server/server.js
@@ -46,6 +46,21 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then(todo => {
+        todo ? res.send(todo) : res.status(404).send;
+    }).catch(err => {
+        res.status(400).send();
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`Started up at port ${port}`)
