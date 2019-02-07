2a9f37a86b2f2b4fbb69bf7acfdd4f51b5071198
Eder Ramirez <documentador1@telecomo.com.mx>
2019-02-07 11:58:29 -0600

    Private routes and auth middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const { ObjectID } = require('mongodb');
 let { mongoose } = require('./db/mongoose');
 let { Todo } = require('./models/todo');
 let { User } = require('./models/user');
+let { authenticate } = require('./middleware/authenticate');
 
 let app = express();
 const port = process.env.PORT;
@@ -106,6 +107,12 @@ app.post('/users', (req, res) => {
     });
     
 });
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+})
 
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
