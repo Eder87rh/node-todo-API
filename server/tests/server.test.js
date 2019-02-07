3ac941c377bd727a150485a8f71b8037b807a861
Eder Ramirez <documentador1@telecomo.com.mx>
2019-02-07 13:55:11 -0600

    Fixing timeout error in mocha

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -16,11 +16,9 @@ const todos = [{
 }]
 
 beforeEach((done) => {
-    Todo.remove({})
-    .then(() => {
-        return Todo.insertMany(todos)
-    })
-    .then(() => done())
+    Todo.remove({}).then(() => {
+      return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
